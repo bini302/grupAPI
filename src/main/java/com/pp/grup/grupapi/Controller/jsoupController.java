27373d10b8e9dc48bfbfc6a03dc4237062fcc7c4
bini302 <bini30275@gmail.com>
2023-09-19 12:52:58 +0900

    크롤링

--- src/main/java/com/pp/grup/grupapi/Controller/jsoupController.java
+++ src/main/java/com/pp/grup/grupapi/Controller/jsoupController.java
@@ -1,48 +1,35 @@
 package com.pp.grup.grupapi.Controller;
 
-import com.pp.grup.grupapi.Entity.Product;
-import com.pp.grup.grupapi.Service.jsoupService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.util.UriUtils;
 
+import com.pp.grup.grupapi.Entity.Product;
+import com.pp.grup.grupapi.Service.JsoupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-@Controller
-public class jsoupController {
+
+@RestController
+@Transactional
+public class JsoupController {
+
     @Autowired
-    private jsoupService jsoupService;
+    private JsoupService jsoupService;
 
-    @GetMapping("/crawling/main")
-    public String search(){
-        return "crawling";
+    @PostMapping("/api/searchSubmit")
+    public ResponseEntity<List<Product>> handleSubmit(@RequestBody String searchName) throws IOException {
+//        System.out.println("들어온 이름 :  " + searchName);
+        String stringWithQuotes = searchName;
+        String stringWithoutQuotes = stringWithQuotes.replace("\"", "");
+//        System.out.println("이름 확인용 :  " + stringWithoutQuotes);
+        jsoupService.searchPlant(stringWithoutQuotes);
+
+        List<Product> entities = jsoupService.getProductsBySearchName(stringWithoutQuotes);
+
+        return ResponseEntity.ok(entities);
     }
-
-    //크롤링 검색을 한 이후 결과 값을 보여줌
-    @PostMapping("/searchSubmit")
-    public String handleSubmit(@RequestParam("searchKeyword") String searchKeyword) throws IOException {
-        jsoupService.searchPlant(searchKeyword);
-        String searchName = UriUtils.encode(searchKeyword, StandardCharsets.UTF_8);
-        return "redirect:/entities?searchName=" + searchName;
-    }
-
-    @GetMapping("/entities")
-    public String getEntities(@RequestParam("searchName") String searchName, Model model) {
-        List<Product> entities = jsoupService.getProductsBySearchName(UriUtils.decode(searchName, StandardCharsets.UTF_8));
-        model.addAttribute("entities", entities);
-        return "jsoupresult";
-    }
-
-    @PostMapping("/deleteEntity")
-    public String deleteEntity(@RequestParam("searchKeyword") String searchKeyword) {
-        jsoupService.deleteBySearchName(searchKeyword);
-        return "redirect:crawling";
-    }
-
 }
+
